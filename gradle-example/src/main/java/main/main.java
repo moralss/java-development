ecaf19cabef8c1869a4fd388479057c73bcf2f5e
moralss <jeramoral@gmail.com>
2019-02-22 08:31:27 +0200

    changed file structure

--- gradle-example/src/main/java/main/main.java
+++ gradle-example/src/main/java/main/main.java
@@ -1,4 +1,10 @@
 package main;
 
-public class main {
+
+ls
+    public class main {
+        public static void  main(String[] args){
+            System.out.println("Hello wolrd");
+
+        }
 }
