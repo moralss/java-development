7823dfb725c924e0637cdfbd44b0362d5fad2539
moralss <jeramoral@gmail.com>
2019-03-14 12:23:21 +0200

    Created an array and performed basiced array methods

---
+++ kata/array/src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        ArrayList<String> names = new ArrayList<String>();
+        names.add("Moral");
+        names.add("Zulu");
+        names.add("Sbu");
+        // get a value
+        String name = names.get(1);
+        for (String student : names) {
+            System.out.println(student);
+        }
+
+        // update a value
+        names.set(1, "Fred");
+
+
+        // removing a value
+        names.remove(2);
+
+        Collections.sort(names);
+
+        System.out.println("fetched name" + " " + name);
+        System.out.println(names);
+    }
+}
