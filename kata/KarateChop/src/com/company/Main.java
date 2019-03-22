e031bc16a714076ba7de2c26bb7315db48b848d0
moralss <jeramoral@gmail.com>
2019-03-22 10:10:10 +0200

    added a kata karateChop

---
+++ kata/KarateChop/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    private static ArrayList<Object> Lists;
+
+    public static void main(String[] args) {
+	// write your code here
+        int[] data= {40,50,10,30,20,5};
+        chop(1, data);
+    }
+
+
+    public static String chop(int checkValue , int[] array) {
+//        ArrayList<String> obj = new ArrayList<String>(
+//                Arrays.asList("moral", "jera", "uzl", ....so on));
+        return "moral";
+    }
+}
