fd57dd8d1cc0b7e6590835da3f377786772e931a
moralss <jeramoral@gmail.com>
2019-03-25 15:17:48 +0200

    Added a new kata

--- kata/KarateChop/src/com/company/Main.java
+++ kata/KarateChop/src/com/company/Main.java
@@ -5,18 +5,27 @@ import java.util.List;
 
 public class Main {
 
-    private static ArrayList<Object> Lists;
+    private static ArrayList<Integer> Lists = new ArrayList<>();
 
     public static void main(String[] args) {
-	// write your code here
-        int[] data= {40,50,10,30,20,5};
-        chop(1, data);
+        // write your code here
+        int[] data = {40, 50, 10, 30, 20, 5};
+        System.out.println(chop(50 , data));
     }
 
 
-    public static String chop(int checkValue , int[] array) {
+    public static int chop(int checkValue, int[] array) {
+        int value = -1;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == checkValue) {
+                return i;
+            } else {
+                continue;
+            }
+        }
+        return value;
 //        ArrayList<String> obj = new ArrayList<String>(
 //                Arrays.asList("moral", "jera", "uzl", ....so on));
-        return "moral";
+//        return "moral";
     }
 }
