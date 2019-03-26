bcea2545774f00e855b94d57e38647db7e3e2865
moralss <jeramoral@gmail.com>
2019-03-26 08:52:15 +0200

    created a class for count most acurring

--- kata/countMostAcurring/src/com/company/CountMostAcurring.java
+++ kata/countMostAcurring/src/com/company/CountMostAcurring.java
@@ -1,5 +1,27 @@
 package com.company;
+public class CountMostAcurring {
 
-public class CountMostAcurring{
+    public int getTotalCount() {
 
+        int[] numbers = {1, 2, 3, 2};
+        int count = 0;
+        int highestValue = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+
+            int digit = numbers[i];
+
+            for (int j = 0; j < numbers.length; j++) {
+                if (digit == numbers[j]) {
+                    count++;
+
+                }
+
+                if(count > highestValue){
+                    highestValue = count;
+                }
+            }
+        }
+        return highestValue;
+    }
 }
--- kata/countMostAcurring/src/com/company/Main.java
+++ kata/countMostAcurring/src/com/company/Main.java
@@ -4,7 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-
+    CountMostAcurring count = new CountMostAcurring();
+    count.getTotalCount();
+//        System.out.println();
     }
 }
 
