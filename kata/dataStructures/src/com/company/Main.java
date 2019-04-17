5c189ec917be9ec6db661c3079d52696a93e1694
moralss <jeramoral@gmail.com>
2019-04-17 15:24:36 +0200

    Created a function that  generates a random

---
+++ kata/dataStructures/src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        LinkedList<String> linkedlist = new LinkedList<>();
+        Map<String, String> names = new HashMap<>();
+        Set<String> members = new HashSet<>();
+        generateRandomPassword(9);
+
+    }
+
+    public static void generateRandomPassword(Integer length) {
+        String characters = "ABCDEFGHIJKLOMOPQRSTUVWXYZ";
+        char [] valuess = new char[length];
+        List<Character> passwordChar = new ArrayList<>();
+        int randomNumber;
+
+        for(int i = 0 ; i < length ; i++){
+            randomNumber = (int) (Math.random() * characters.length());
+            passwordChar.add(characters.charAt(randomNumber));
+
+        }
+    }
+}
+
+
