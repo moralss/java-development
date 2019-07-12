a15e3f6813c4758de68e4f62f9f147b33c9e3c33
moralss <jeramoral@gmail.com>
2019-07-12 10:34:07 +0200

    Created a simple hibernate  example

---
+++ project/hibernate/src/main/java/apphibernate/User.java
@@ -0,0 +1,37 @@
+package apphibernate;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "friends", catalog = "test")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+
+    public User(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
---
+++ project/hibernate/src/main/java/apphibernate/UserRepository.java
@@ -0,0 +1,11 @@
+package apphibernate;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public  interface UserRepository extends JpaRepository<User, Integer> {
+    List<User> findByName(String name);
+    User save(User user);
+}
